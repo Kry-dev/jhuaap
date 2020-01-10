c404c997d7bfd736c39405e25af96c0ee6dd88b5
kry-dev <yaroslavkryvda@gmail.com>
2020-01-10 19:23:26 +0200

    add student page

---
+++ src/blocks/modules/student-list/student-list.js
@@ -0,0 +1,13 @@
+modules.define('student-list', ['i-bem-dom'], function(provide, bemDom) {
+
+provide(bemDom.declBlock(this.name, {
+    onSetMod: {
+        js: {
+            inited: function() {
+                
+            }
+        }
+    }
+}));
+
+});
